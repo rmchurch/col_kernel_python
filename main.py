d9b30443f075712802e9d8c9f41ee0028e0429b1
rmchurch <1577108+rmchurch@users.noreply.github.com>
2022-02-01 17:31:17 -0500

    Updated for ion-only, and using fhat

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from modules import sml_param,f0_param,col_f_param
 from ml_collisions import MLCollisions
 import matplotlib.pyplot as plt; plt.ion()
 import torch
+import torch.nn.functional as F
 
 #TODO put into arg parser
 plot=False #True
@@ -23,32 +24,31 @@ nml['col_f_param'] = col_f_param().__dict__
 nml['col_f_param'].update(nml_in['col_f_param'])
 
 sml = nml['sml_param']
-sml['sml_mass'] = [m*sml['sml_prot_mass'] for m in sml['sml_mass']]
-sml['sml_charge'] = [q*sml['sml_e_charge'] for q in sml['sml_charge']]
+sml['sml_mass'] = sml['sml_prot_mass']*sml['sml_mass']
+sml['sml_charge'] = sml['sml_e_charge']*sml['sml_charge']
 ## Compute electron collision time using the ion-electron Coulomb logarithm
-l_ie  = 24. - np.log(np.sqrt(sml['sml_den'][0]*1e-6)/sml['sml_t_ev'][0])
-sml_tau_e = np.sqrt(sml['sml_t_ev'][0]**3)/(2.91e-6*sml['sml_den'][0]*1e-6*l_ie)
-sml['sml_dt'] = sml['sml_dt']*sml_tau_e
+l_ii  = 24. - np.log(np.sqrt(sml['sml_den']*1e-6)/sml['sml_t_ev']) #TODO: update with actual l_ii, this is l_ie with ion
+sml_tau_i = np.sqrt(sml['sml_t_ev']**3)/(2.91e-6*sml['sml_den']*1e-6*l_ii)
+sml['sml_dt'] = sml['sml_dt']*sml_tau_i
+Nsp = sml['sml_nsp']-sml['sml_isp']+1
 
 
 ##f0_init
 f0 = nml['f0_param']
-Nsp = f0['f0_nsp']-f0['f0_isp']+1
-if not type(f0['f0_velo_aniso'])==list: f0['f0_velo_aniso'] = Nsp*[f0['f0_velo_aniso']]
 f0_dsmu = f0['f0_smu_max']/float(f0['f0_nmu']) 
 f0_dvp = f0['f0_vp_max']/float(f0['f0_nvp']) 
 vperp = np.arange(f0['f0_nmu']+1)*f0_dsmu
 vpara = np.arange(-f0['f0_nvp'],f0['f0_nvp']+1)*f0_dvp
 f0_nnode = 1 #hardcode for now
 #f0_isp is 0 if electrons, 1 if no electrons
-f0_f = np.zeros((Nsp,vperp.size,f0_nnode,vpara.size))
-for isp in range(f0_f.shape[0]):
+f0_f = np.zeros((f0_nnode, Nsp, vperp.size, vpara.size))
+for isp in range(f0_f.shape[1]):
     for inode in range(f0_nnode):
-        energy_para = 0.5*(vpara - sml['sml_flow'][isp])**2./f0['f0_velo_aniso'][isp]
+        energy_para = 0.5*(vpara - sml['sml_flow'])**2./f0['f0_velo_aniso']
         energy_perp = 0.5*vperp**2.
         energy = np.einsum('i,j->ij',energy_perp,np.ones(energy_para.shape)) + \
                  np.einsum('i,j->ij',np.ones(energy_perp.shape),energy_para)
-        f0_f[isp,:,inode,:] = vperp[:,np.newaxis]*sml['sml_den'][isp]/(np.sqrt(f0['f0_velo_aniso'][isp])*sml['sml_t_ev'][isp])*np.exp(-energy)
+        f0_f[inode, isp, :,:] = vperp[:,np.newaxis]*sml['sml_den']/(np.sqrt(f0['f0_velo_aniso'])*sml['sml_t_ev'])*np.exp(-energy)
 if use_jet:
     #try instead data directly from JET simulation
     import h5py
@@ -71,32 +71,36 @@ f0_f_init = f0_f.copy()
 
 
 ##collison operator setup
-file_log = 'checkpoint_log.pickle'
-file_model = 'checkpoint.pth.tar'
-file_stats = 'hdf_stats.h5'
-collisions = MLCollisions(file_model,file_log,file_stats,normalize=False)
+file_model = '/scratch/gpfs/rmc2/ml_collisions/mic_parallel/model_best.254407.pth.tar'
+collisions = MLCollisions(file_model, channels=1)
 
 
 ## moment function
 #this leaves out a vperp, since its cancelled with the vol factor
-conv_factor = 1./np.sqrt(np.array(sml['sml_t_ev'])*(2*np.pi*sml['sml_e_charge']/np.array(sml['sml_mass']))**3)
+conv_factor = np.array([1./np.sqrt(np.array(sml['sml_t_ev'])*(2*np.pi*sml['sml_e_charge']/np.array(sml['sml_mass']))**3)])
 vperp1 = vperp.copy()
 vperp1[0] = vperp1[1]/3.
+
+
 #vperp1[-1] = vperp1[1]*(vperp1.size-1./3.)
 #this leaves out a vperp, since its cancelled with the conv_factor factor
 #TODO: The ends have different values, decide what to do
 m = np.array(sml['sml_mass']); q = np.array(sml['sml_charge'])
-vth = np.sqrt(np.array(sml['sml_t_ev'])*sml['sml_ev2j']/m)
-vol = 2*np.pi*f0_dsmu*f0_dvp*np.einsum('i,j->ij',vth**3.,np.ones(vperp.shape))
+vth = np.array([np.sqrt(np.array(sml['sml_t_ev'])*sml['sml_ev2j']/m)])
+vol = 2*np.pi*f0_dsmu*f0_dvp*np.einsum('i,j->ij',vth**3.,vperp)
 vol[:,0] = 0.5/3.*vol[:,1] #would the 1/3 be cancelled by conv_factor?
 vol[:,-1] = 0.5*(vperp.size-1./3.)/(vperp.size-1.)*vol[:,-2]
+
+#convert f0_f to the collision operator representation
+fhat = np.einsum('ijkl,j,k->ijkl',f0_f,conv_factor,1./vperp1)
+
 def calcMoments(f):
-    den = np.einsum('ijkl,i,ij->ik',f,conv_factor,vol)
-    upar = np.einsum('l,ijkl,i,ij->ik',vpara,f,conv_factor,vol)/den
-    prefac = 0.5*np.array(sml['sml_t_ev']) #this is the 1/2*m*vth^2 factor, but normalizes out to this
-    Tperp = np.einsum('i,j,ijkl,i,ij->ik',prefac,vperp1**2.,f,conv_factor,vol)/den
-    Tpara = 2.*np.einsum('i,l,ijkl,i,ij->ik',prefac,vpara**2.,f,conv_factor,vol)/den
-    entropy = np.einsum('ijkl,i,ij->ik',-np.log(f+1e-16)*f,conv_factor,vol)
+    den = np.einsum('kijl,ij->ik',f,vol)
+    upar = np.einsum('l,kijl,ij->ik',vpara,f,vol)/den
+    prefac = np.array([0.5*np.array(sml['sml_t_ev'])]) #this is the 1/2*m*vth^2 factor, but normalizes out to this
+    Tperp = np.einsum('i,j,kijl,ij->ik',prefac,vperp1**2.,f,vol)/den
+    Tpara = 2.*np.einsum('i,l,kijl,ij->ik',prefac,vpara**2.,f,vol)/den
+    entropy = np.einsum('kijl,ij->ik',-np.log(f+1e-16)*f,vol)
     return den,upar,Tperp,Tpara,entropy
 
 
@@ -104,7 +108,8 @@ def calcMoments(f):
 ions = True
 electrons = True
 if collisions.channels==1: electrons=False
-den = np.zeros((f0_f.shape[0],f0_f.shape[2],sml['sml_nstep']))
+#Ngrid, Nsp
+den = np.zeros((f0_f.shape[1],f0_f.shape[0],sml['sml_nstep']))
 upar = np.zeros(den.shape)
 Tperp = np.zeros(den.shape); Tpara = np.zeros(den.shape)
 entropy = np.zeros(den.shape)
@@ -114,44 +119,22 @@ dTperp = np.zeros(den.shape); dTpara = np.zeros(den.shape)
 if plot: fig,ax = plt.subplots(2,4)
 for i in range(sml['sml_nstep']):
     print('Start step %d' % i)
-    den[...,i],upar[...,i],Tperp[...,i],Tpara[...,i],entropy[...,i] = calcMoments(f0_f)
-    print('n: %.2e \t upar: %.2e \t Tperp: %.2e \t Tpara: %.2e ' % (den[1,0,i],upar[1,0,i],Tperp[1,0,i],Tpara[1,0,i]))
+    den[...,i],upar[...,i],Tperp[...,i],Tpara[...,i],entropy[...,i] = calcMoments(fhat)
+    print('n: %.2e \t upar: %.2e \t Tperp: %.2e \t Tpara: %.2e ' % (den[0,0,i],upar[0,0,i],Tperp[0,0,i],Tpara[0,0,i]))
 
-    fpre = collisions.preprocess(torch.from_numpy(f0_f))
-    if normalize:
-        fnorm = collisions.normalize_f(fpre)
-    else:
-        fnorm = fpre
-    fdfnorm = collisions.forward_nn(fnorm.contiguous())
-    if normalize:
-        dfout = collisions.unnormalize_fdf(fdfnorm) - fpre
-    else:
-        dfout = fdfnorm
-    df = collisions.postprocess(dfout).contiguous().detach().numpy()
+    temp = (Tpara[...,i] + 2*Tperp[...,i])/3.
+    fin = F.pad(torch.from_numpy(fhat), (1,0,0,0),mode='replicate') 
+    fdf = collisions.forward(fin.cuda(), torch.from_numpy(temp).cuda()).detach().cpu().numpy() #pre- and post-processing in forward() pass
+    df = np.maximum(fdf[...,:-1], fhat) - fhat
 
-    
     dden[...,i],dupar[...,i],dTperp[...,i],dTpara[...,i],_ = calcMoments(df)
-    if electrons:
-        if plot:
-            ax[0,0].contourf(f0_f[0,:,0,:],100)
-            ax[0,1].contourf(fnorm[0,0,:,:].detach().numpy(),100)
-            ax[0,2].contourf(fdfnorm[0,0,:,:].detach().numpy(),100)
-            ax[0,3].contourf(df[0,:,0,:],100)
-        f0_f[0,...] += df[0,...]
-    if ions:
-        if plot:
-            ax[1,0].contourf(f0_f[1,:,0,:],100)
-            ax[1,1].contourf(fnorm[0,1,:,:].detach().numpy(),100)
-            ax[1,2].contourf(fdfnorm[0,1,:,:].detach().numpy(),100)
-            ax[1,3].contourf(df[1,:,0,:],100)
-        f0_f[1,...] += df[collisions.channels-1,...]
     if plot:
         plt.suptitle('Step %d' % i)
         plt.draw()
         plt.pause(0.1)
     #remove negative
-    #f0_f[f0_f<0] = 1e-16
-    #f0_f += df
+    fhat[fhat<0] = 1e-16
+    fhat += df
 
 #save moment data
 np.savez('col_output.npz',den=den,upar=upar,Tperp=Tperp,Tpara=Tpara,
