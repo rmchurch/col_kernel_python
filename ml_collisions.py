49fed633c08862622a30fe69b789a21615f13534
rmchurch <1577108+rmchurch@users.noreply.github.com>
2022-03-30 17:49:13 -0400

    Lay out better the process, saving the mean/std for postprocess

--- ml_collisions.py
+++ ml_collisions.py
@@ -24,7 +24,25 @@ class MLCollisions(torch.nn.Module):
         model.load_state_dict(new_state_dict)
         model.eval()
         self.model = model
+        self.mean = 0.0
+        self.std = 0.0
+
+    def stats(self,f):
+        self.mean = f.mean(dim=(2,3),keepdim=True)
+        self.std = f.std(dim=(2,3),keepdim=True)
+
+    def preprocess(self,f,temp):
+        fnorm = ((f - self.mean)/self.std).float()
+        tempnorm = (temp/5400).float()
+        return fnorm, tempnorm
+
+    def postprocess(self,fnorm):
+        return fnorm*self.std + self.mean
 
     def forward(self,f,temp):
-        return self.model((f - f.mean(dim=(2,3),keepdim=True)/f.std(dim=(2,3),keepdim=True)).float(), (temp/5400).float())*f.std(dim=(2,3),keepdim=True) + f.mean(dim=(2,3),keepdim=True)
+        self.stats(f)
+        fnorm, tempnorm = self.preprocess(f,temp)
+        fdfnorm = self.model(fnorm, tempnorm)
+        fdf = self.postprocess(fdfnorm)
+        return fdf
 
