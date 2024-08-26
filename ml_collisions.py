f161021151f6536449f7dc6770d96f48278c8c4f
Hong Zhang <hongzhang@anl.gov>
2024-08-26 10:04:46 -0500

    Black format

--- ml_collisions.py
+++ ml_collisions.py
@@ -3,28 +3,47 @@
 import torch
 import torch.nn.functional as F
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 from models import ReSeg
 import pickle
 import h5py
 import numpy as np
 
+
 class MLCollisions(torch.nn.Module):
 
-    def __init__(self, file_model, device, channels=1, dropout=0.5, train_df=False, minmax=False, div_log_tran=False, div_log_tran_divisor=1e10, scaling='normalize', crop=None, pad=None, fmax=None, fmin=None):
+    def __init__(
+        self,
+        file_model,
+        device,
+        channels=1,
+        dropout=0.5,
+        train_df=False,
+        minmax=False,
+        div_log_tran=False,
+        div_log_tran_divisor=1e10,
+        scaling="normalize",
+        crop=None,
+        pad=None,
+        fmax=None,
+        fmin=None,
+    ):
         super().__init__()
         self.channels = channels
-        out = torch.load(file_model,map_location=device)
-        model = ReSeg(channels=channels,dropout=dropout).to(device)
-        model = torch.compile(model)
-        state_dict = out['state_dict']
-        torch.nn.modules.utils.consume_prefix_in_state_dict_if_present(state_dict, "module.")
-        #from collections import OrderedDict
-        #new_state_dict = OrderedDict()
-        #for k, v in state_dict.items():
+        out = torch.load(file_model, map_location=device)
+        model = ReSeg(channels=channels, dropout=dropout).to(device)
+        # model = torch.compile(model)
+        state_dict = out["state_dict"]
+        torch.nn.modules.utils.consume_prefix_in_state_dict_if_present(
+            state_dict, "module."
+        )
+        # from collections import OrderedDict
+        # new_state_dict = OrderedDict()
+        # for k, v in state_dict.items():
         #    name = k[7:] # remove `module.`
         #    new_state_dict[name] = v
-        #model.load_state_dict(new_state_dict)
+        # model.load_state_dict(new_state_dict)
         model.load_state_dict(state_dict)
         model.eval()
         self.model = model
@@ -41,58 +60,62 @@ class MLCollisions(torch.nn.Module):
         self.fmax = fmax
         self.fmin = fmin
 
-    def stats(self,f):
+    def stats(self, f):
         if self.minmax:
             if torch.is_tensor(f):
-                self.mean = f.amin(dim=(2,3),keepdims=True)
-                self.std = f.amax(dim=(2,3),keepdims=True) - self.mean
+                self.mean = f.amin(dim=(2, 3), keepdims=True)
+                self.std = f.amax(dim=(2, 3), keepdims=True) - self.mean
             else:
-                self.mean = np.min(f,axis=(2,3),keepdims=True)
-                self.std = np.max(f,axis=(2,3),keepdims=True) - self.mean
+                self.mean = np.min(f, axis=(2, 3), keepdims=True)
+                self.std = np.max(f, axis=(2, 3), keepdims=True) - self.mean
         else:
             if torch.is_tensor(f):
-                self.mean = f.mean(dim=(2,3),keepdim=True)
-                self.std = f.std(dim=(2,3),keepdim=True,unbiased=False)+1e-15 # make it consistent with the numpy implementation
+                self.mean = f.mean(dim=(2, 3), keepdim=True)
+                self.std = (
+                    f.std(dim=(2, 3), keepdim=True, unbiased=False) + 1e-15
+                )  # make it consistent with the numpy implementation
             else:
-                self.mean = np.mean(f,axis=(2,3),keepdims=True)
-                self.std = np.std(f,axis=(2,3),keepdims=True)+1e-15
+                self.mean = np.mean(f, axis=(2, 3), keepdims=True)
+                self.std = np.std(f, axis=(2, 3), keepdims=True) + 1e-15
 
-    def print_stats(self,f):
+    def print_stats(self, f):
         torch.set_printoptions(8)
         print(self.mean)
         print(self.std)
         if self.minmax:
             if torch.is_tensor(f):
-                mean = f.amin(dim=(2,3),keepdims=True)
-                std = f.amax(dim=(2,3),keepdims=True) - mean
+                mean = f.amin(dim=(2, 3), keepdims=True)
+                std = f.amax(dim=(2, 3), keepdims=True) - mean
             else:
-                mean = np.min(f,axis=(2,3),keepdims=True)
-                std = np.max(f,axis=(2,3),keepdims=True) - mean
+                mean = np.min(f, axis=(2, 3), keepdims=True)
+                std = np.max(f, axis=(2, 3), keepdims=True) - mean
         else:
             if torch.is_tensor(f):
-                mean = f.amean(dim=(2,3),keepdim=True)
-                std = f.std(dim=(2,3),keepdim=True,unbiased=False)+1e-15 # make it consistent with the numpy implementation
+                mean = f.amean(dim=(2, 3), keepdim=True)
+                std = (
+                    f.std(dim=(2, 3), keepdim=True, unbiased=False) + 1e-15
+                )  # make it consistent with the numpy implementation
             else:
-                mean = np.mean(f,axis=(2,3),keepdims=True)
-                std = np.std(f,axis=(2,3),keepdims=True)+1e-15
+                mean = np.mean(f, axis=(2, 3), keepdims=True)
+                std = np.std(f, axis=(2, 3), keepdims=True) + 1e-15
         print(mean)
         print(std)
 
     def preprocess(self, f):
         if self.div_log_tran:
             f = self.div_log_transform(f, self.div_log_tran_divisor)
-        if self.scaling == 'normalize':
+        if self.scaling == "normalize":
             self.stats(f)
-            f = (f - self.mean)/self.std
-        if self.scaling == 'normalizeall':
-            f = (f - self.fmin)/(self.fmax-self.fmin)
+            f = (f - self.mean) / self.std
+        if self.scaling == "normalizeall":
+            f = (f - self.fmin) / (self.fmax - self.fmin)
         return f
 
     def postprocess(self, f):
-        if self.scaling == 'normalizeall':
-            f = f*(self.fmax-self.fmin) + self.fmin
-        if self.scaling == 'normalize':
-            f = f*self.std + self.mean
+        if self.scaling == "normalizeall":
+            f = f * (self.fmax - self.fmin) + self.fmin
+        if self.scaling == "normalize":
+            f = f * self.std + self.mean
         if self.div_log_tran:
             f = self.div_log_untransform(f, self.div_log_tran_divisor)
         return f
@@ -100,8 +123,8 @@ class MLCollisions(torch.nn.Module):
     def forward(self, fnorm, coulomb_factor):
         fnorm = self.pad(fnorm)
         if self.train_df:
-            dt = 1.34e-7
-            fdfnorm = fnorm + dt*coulomb_factor*self.model(fnorm)
+            dt = 1.34048e-07
+            fdfnorm = fnorm + dt * coulomb_factor * self.model(fnorm)
         else:
             fdfnorm = self.model(fnorm)
         fdfnorm = self.crop(fdfnorm)
@@ -109,20 +132,20 @@ class MLCollisions(torch.nn.Module):
 
     def div_log_transform(self, X, divisor):
         if torch.is_tensor(X):
-            Xtran = torch.log(X/divisor)
+            Xtran = torch.log(X / divisor)
         else:
-            Xtran = np.log(X/divisor)
+            Xtran = np.log(X / divisor)
         return Xtran
 
     def div_log_untransform(self, Xtran, divisor):
         if torch.is_tensor(Xtran):
-            X = torch.exp(Xtran)*divisor
+            X = torch.exp(Xtran) * divisor
         else:
-            X = np.exp(Xtran)*divisor
+            X = np.exp(Xtran) * divisor
         return X
 
     def _crop(self, data):
-        return data[...,:-1]
+        return data[..., :-1]
 
     def _pad(self, data):
-        return F.pad(data, (0,1,0,0), mode='replicate')
+        return F.pad(data, (0, 1, 0, 0), mode="replicate")
